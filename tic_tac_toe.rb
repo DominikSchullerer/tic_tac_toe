a42b04727a0a817e5779bf155de871c2debafe74
Cheranor <89839452+Cheranor@users.noreply.github.com>
2023-02-18 00:29:48 +0100

    Implement Board and its instance methods draw and update

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -12,18 +12,38 @@ class Player
   end
 
   def get_move
-    move = Array.new(2)
+    move = Array.new(3)
     puts 'Enter your move'
     print 'Row: '
     move[0] = gets.chomp.to_i
     print 'Column: '
     move[1] = gets.chomp.to_i
+    move[2] = sign
     move
   end
 end
 
 # Board
 # Board stores the gamestate. It can draw the board to the console and update the gamestate.
+class Board
+  attr_reader :grid
+
+  def initialize
+    @grid = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]
+  end
+
+  def update(move)
+    @grid[move[0]][move[1]] = move [2]
+  end
+
+  def draw
+    puts "#{@grid[0][0]}|#{@grid[0][1]}|#{@grid[0][2]}"
+    puts '-----'
+    puts "#{@grid[1][0]}|#{@grid[1][1]}|#{@grid[2][2]}"
+    puts '-----'
+    puts "#{@grid[2][0]}|#{@grid[2][1]}|#{@grid[2][2]}"
+  end
+end
 
 # Rules
 # Rules is a module containing methods to check the validity of a move or if the game is won.
